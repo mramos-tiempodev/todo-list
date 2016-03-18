65c8d2b9a4f2837d0fc5533a3adedd6a67873aad
Martin Ramos <mramos@tiempodevelopment.com>
2016-03-17 18:32:39 -0700

    Hello world !!

--- module/Wall/config/module.config.php
+++ module/Wall/config/module.config.php
@@ -1,4 +1,35 @@
 <?php
 return array(
-
+    'router' => array(
+        'routes' => array(
+            'wall' => array(
+                'type' => 'Zend\Mvc\Router\Http\Segment',
+                'options' => array(
+                    'route'    => '/:username',
+                    'constraints' => array(
+                        'username' => '\w+'
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Wall\Controller\Index',
+                        'action'     => 'index',
+                    ),
+                ),
+            )
+        ),
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'Wall\Controller\Index' => 'Wall\Controller\IndexController'
+        ),
+    ),
+    'view_manager' => array(
+//        'display_not_found_reason' => true,
+//        'display_exceptions'       => true,
+//        'doctype'                  => 'HTML5',
+//        'not_found_template'       => 'error/404',
+//        'exception_template'       => 'error/index',
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
 );
--- module/Wall/src/Controller/IndexController.php
+++ module/Wall/src/Controller/IndexController.php
@@ -6,4 +6,8 @@ use Zend\Mvc\Controller\AbstractActionController;
 class IndexController extends AbstractActionController
 {
 
+    public function indexAction()
+    {
+        var_dump('Hello world');
+    }
 } 
